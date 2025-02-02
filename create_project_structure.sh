dd8b60e0dfeb1abbd962c89177e5d7bca5483127
BTCElectrician <Velardecollin@gmail.com>
2025-02-01 22:00:59 -0600

    chore: remove project structure creation script
    - Deleted create_project_structure.sh script
    - No longer needed as project structure is now established
    - Simplifies repository by removing redundant shell script

--- create_project_structure.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p data_processing azure_search azure_function
-touch main.py requirements.txt
-touch data_processing/pdf_extractor.py data_processing/text_chunker.py
-touch azure_search/index_creator.py azure_search/data_indexer.py
-touch azure_function/function_app.py 
